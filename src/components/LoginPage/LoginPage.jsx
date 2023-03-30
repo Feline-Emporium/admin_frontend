05132887af3b6ad4b968068b6cd96d0e1974eb05
Adam <hviidadam@gmail.com>
2023-03-30 16:01:33 +0200

    auth

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 import { Typography, TextField, Button } from "@mui/material";
+import axios from "axios";
 import "./LoginPage.css";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    await axios
+      .post("http://localhost:3002/login", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        if (res.data === "invalid username or password") {
+          alert("invalid username or password");
+          return;
+        } else {
+          localStorage.setItem("JWT_TOKEN", res.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <div>
--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./UserTable.css";
 
 import DeleteButton from "../DeleteButton";
 
 function UserTable() {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  const [token, setToken] = useState(localStorage.getItem("JWT_TOKEN"));
 
   useEffect(() => {
-    getUsers();
+    if (token === null) {
+      navigate("/login");
+    } else {
+      getUsers();
+    }
   }, []);
 
   async function getUsers() {
     await axios
-      .get("http://localhost:3002/users/all")
+      .post(
+        "http://localhost:3002/users/all",
+        {
+          JWT_TOKEN: token,
+        },
+        {
+          headers: {
+            JWT_TOKEN: token,
+          },
+        }
+      )
       .then((res) => {
         setUsers(res.data);
       })
